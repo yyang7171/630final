888481daed5dca541d18d8be3237a05682e0c704
Yabing Yang <98364345+yyang7171@users.noreply.github.com>
2024-04-18 15:36:45 -0400

    gsap

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 async function loadData(url) {
-    const results = await fetch(url);
-    const storedList = await results.json();
-  
-    return storedList;
-  }
-  
-  async function mainEvent() {
-    const data = await loadData(
-      "https://api.artic.edu/api/v1/artworks/search?params=%7B%22q%22%3A%22cats%22%2C%22query%22%3A%7B%22term%22%3A%7B%22is_public_domain%22%3Atrue%7D%7D%7D"
-    );
-  }
+  const results = await fetch(url)
+  const storedList = await results.json()
+  return storedList
+}
+
+async function mainEvent() {
+  const data = await loadData(
+    'https://api.artic.edu/api/v1/artworks/search?params=%7B%22q%22%3A%22cats%22%2C%22query%22%3A%7B%22term%22%3A%7B%22is_public_domain%22%3Atrue%7D%7D%7D'
+  )
   console.table(data)
-  document.addEventListener("DOMContentLoaded", async () => mainEvent());
-  
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => mainEvent())
